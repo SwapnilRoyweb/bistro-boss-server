a1c9dddbce8d6029ac7837f425d6dae6e1821b71
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-06-06 15:28:34 +0600

    my cart delete function also added to server

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ad0f77m.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -46,7 +46,7 @@ async function run() {
     // Cart Collection
 
     // Cart collection api
-    app.get ('/carts', async (req, res) => {
+    app.get('/carts', async (req, res) => {
       const email = req.query.email;
       if(!email){
         res.send([]);
@@ -60,6 +60,13 @@ async function run() {
       const item = req.body;
       // console.log(item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
