c3c43eb52dee17bb1c2123bc6024a54433f5e49e
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-06-08 22:43:14 +0600

    admin post req function added to server

--- index.js
+++ index.js
@@ -82,6 +82,20 @@ async function run() {
       res.send(result);
     });
 
+    // security layer: verifyJWT -> email same -> check admin
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+       return res.send({admin: false});
+      }
+
+      const query = {email : email};
+      const user = await usersCollection.findOne(query);
+      const result = {admin : user?.role === 'admin'};
+      res.send(result);
+    })
+
     app.patch('/users/admin/:id', async (req, res) => {
 
       const id = req.params.id;
